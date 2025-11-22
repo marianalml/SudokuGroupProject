edc76ea2a8c3434762d78a8b368f757b553d1778
Julia Jones <julia@Julias-MacBook-Pro.local>
2025-11-22 16:35:55 -0500

    added board.py

---
+++ board.py
@@ -0,0 +1,98 @@
+import pygame
+from cell import Cell, cell_size
+
+board_size = cell_size * 9
+
+class Board:
+
+    def draw(self):
+        for r in range(9):
+            for c in range(9):
+                self.cells[r][c].draw()
+
+        for i in range (10):
+            thickness = 3 if i % 3 == 0 else 1
+
+            pygame.draw.line(
+                self.screen,(0,0,0),
+                (0, i * cell_size),
+                (board_size, i * cell_size),
+                thickness
+            )
+
+            pygame.draw.line(
+                self.screen,(0,0,0),
+                (i * cell_size, 0),
+                (i * cell_size, board_size),
+                thickness
+            )
+
+    def select(self, row, col):
+        if self.selected is not None:
+            old_r, old_c = self.selected
+            self.cells[old_r][old_c].selected = False
+
+        self.selected = (row, col)
+        self.cells[row][col].selected = True
+
+    def click(self, x, y):
+        if x < 0 or x >= board_size or y < 0 or y >= board_size:
+            return None
+
+        row = y // cell_size
+        col = x // cell_size
+        return (row, col)
+
+    def sketch(self, value):
+        if self.selected is None:
+            return
+
+        r, c = self.selected
+
+        if self.original[r][c] != 0:
+            return
+
+        self.cells[r][c].set_sketched_value(value)
+
+
+    def place_number(self, value):
+        if self.selected is None:
+            return
+
+        r, c = self.selected
+        if self.original[r][c] != 0:
+            return
+
+        self.cells[r][c].set_cell_value(value)
+        self.update_board()
+
+    def clear(self):
+        if self.selected is None:
+            return
+
+        r, c = self.selected
+
+        if self.original[r][c] != 0:
+            return
+
+        self.cells[r][c].set_cell_value(0)
+        self.cells[r][c].set_sketched_value(0)
+
+    def update_board(self):
+        for r in range(9):
+            for c in range(9):
+                if self.cells[r][c].value == 0:
+                    return False
+        return True
+
+    def check_board(self):
+        if self.solution is None:
+            return False
+
+        for r in range(9):
+            for c in range(9):
+                if self.cells[r][c].value !=  self.solution[r][c]:
+                    return False
+        return True
+
+
