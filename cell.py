4069d4e4cb4280ae88afe605a2aff766fe34b42d
Julia Jones <julia@Julias-MacBook-Pro.local>
2025-11-24 12:38:15 -0500

    centered sketched numbers

--- cell.py
+++ cell.py
@@ -31,12 +31,13 @@ class Cell:
 
         if self.value != 0:
             text = self.main_font.render(str(self.value), True, (0,0,0))
-            text_rect = text_surface = text.get_rect(center =(x + cell_size//2, y + cell_size//2))
+            text_rect = text.get_rect(center =(x + cell_size//2, y + cell_size//2))
             self.screen.blit(text, text_rect)
 
         elif self.sketched_value != 0:
             sketch = self.sketch_font.render(str(self.sketched_value), True, (128,128,128))
-            self.screen.blit(sketch, (x + 5, y + 5))
+            sketch_rect = sketch.get_rect(center=(x + cell_size//2, y + cell_size//2))
+            self.screen.blit(sketch, sketch_rect)
 
         if self.selected:
             pygame.draw.rect(self.screen, (255,0,0), (x, y, cell_size, cell_size),3)
